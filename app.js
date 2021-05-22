d99df244df0e6b2d2047a53b338ff600fb24f8c8
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-05-22 10:43:05 +0500

    user name add and remove from prompt

---
+++ app.js
@@ -0,0 +1,19 @@
+// var cleanestCities = ["karachi", "islamabad", "quetta", "peshawar", "multan"];
+// var cityCheck = prompt("Enter City Name Here")
+// cityCheck = cityCheck.toLowerCase();
+// for (var i = 0; i <= 5; i++) {
+//     if (cityCheck=== cleanestCities[i]) {
+//         alert("It's one of the cleanest cities");
+//     }
+
+// }
+
+// var name = "Syed Tayyab Hussain Pak Fizaiyya"
+// var oldName =prompt("What do you want to remove from Syed Tayyab Hussain Pak Fizaiyya")
+// var index = name.indexOf(oldName)
+// var add = prompt("Waht do want to add in this name")
+// if (index !== -1) {
+//     // console.log(index)
+//     name = name.slice(0, index) + add + name.slice(index + oldName.length)
+//     console.log(name)
+// }
